bcc50afe1163dc5e9d5664ab59d3b5959e063e2e
Júlio Bernardes <juliocd.bernardes@gmail.com>
2024-11-10 17:46:27 -0300

    (crud) - implementing CRUD logic and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,74 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_api_from_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Julio',
+            'email': 'juliocd.bernardes@gmail.com',
+            'password': 'Teste@123',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Julio',
+        'email': 'juliocd.bernardes@gmail.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Julio',
+                'email': 'juliocd.bernardes@gmail.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Julio',
+            'email': 'julioTeste@gmail.com',
+            'password': 'Teste@123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Julio',
+        'email': 'julioTeste@gmail.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
