5e1b2ad56e48ed8a4983b2a07e4005eabdc7083b
jdbernardes <juliocd.bernardes@gmail.com>
2024-11-24 18:01:15 -0300

    Adjusting annotation as per PEP-8

--- fast_api_from_zero/routers/users.py
+++ fast_api_from_zero/routers/users.py
@@ -19,12 +19,14 @@ from fast_api_from_zero.security import get_current_user, get_password_hash
 
 router = APIRouter(prefix='/users', tags=['users'])
 
-Session = Annotated[Session, Depends(get_session)]
-CurrentUser = Annotated[User, Depends(get_current_user)]
+# As a best practice add capital T underscore in
+# the annotated type that I am defining
+T_Session = Annotated[Session, Depends(get_session)]
+T_CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session): # type: ignore comment
+def create_user(user: UserSchema, session: T_Session):
     db_user = session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
@@ -57,7 +59,7 @@ def create_user(user: UserSchema, session: Session): # type: ignore comment
 @router.get('/', response_model=UserList)
 def read_users(
     filter_users: Annotated[FilterPage, Query()],
-    session: Session, # type: ignore comment
+    session: T_Session,
 ):
     users = session.scalars(
         select(User).offset(filter_users.offset).limit(filter_users.limit)
@@ -66,7 +68,7 @@ def read_users(
 
 
 @router.get('/{user_id}', response_model=UserPublic)
-def read_user(user_id: int, session: Session): # type: ignore comment
+def read_user(user_id: int, session: T_Session):
     db_user = session.scalar(select(User).where(User.id == user_id))
     if not db_user:
         raise HTTPException(
@@ -79,8 +81,8 @@ def read_user(user_id: int, session: Session): # type: ignore comment
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session, # type: ignore comment
-    current_user: CurrentUser,
+    session: T_Session,
+    current_user: T_CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
@@ -109,8 +111,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session, # type: ignore comment
-    current_user: CurrentUser,
+    session: T_Session,
+    current_user: T_CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
